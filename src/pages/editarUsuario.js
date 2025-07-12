44fc142e0e35b02d527eddae1f115f7209e9eb9d
valeriap14 <valepb001@gmail.com>
2025-07-11 20:46:23 -0600

    arreglo pages editarPerfil

--- src/pages/editarUsuario.js
+++ src/pages/editarUsuario.js
@@ -1,207 +1,167 @@
-import { useState, useRef, useEffect, useCallback, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import {FaBell, FaUserCircle, FaBars, FaTimes,FaCar, FaHome, FaRoute, FaMoneyBill, FaQuestionCircle} from 'react-icons/fa';
-
-
+import { FaUserCircle } from 'react-icons/fa';
 
 import api from '../api/api';
-import loopLogo from '../assets/loop.png';
 import '../styles/RegistrarUsuario.css';
 import '../styles/editarUsuario.css';
 
+function EditarUsuario() {
+  const { usuario } = useContext(AuthContext);
+  const [password, setPassword] = useState('');
+  const [parametros, setParametro] = useState(false);
+  const [modoEdicion, setModoEdicion] = useState(null);
+  const navigate = useNavigate();
 
-function EditarUsuario(){
+  const [formulario, setFormulario] = useState({
+    nombre: '',
+    apellido: '',
+    telefono: ''
+  });
 
-        const { usuario, inicioDatos} = useContext(AuthContext);
-        const [password, setPassword] = useState();
-        const [parametros, setParametro] = useState(false);
-        const [errors, setErrors] = useState({});
-        const [serverError, setServerError] = useState('');
-        const [modoEdicion, setModoEdicion] = useState(null);
-        const navigate = useNavigate;
-        setErrors({});
-         setServerError('');
+  useEffect(() => {
+    if (usuario) {
+      setFormulario({
+        nombre: usuario.nombre,
+        apellido: usuario.apellido,
+        telefono: usuario.telefono
+      });
+    }
+  }, [usuario]);
 
-        const [formulario, setFormulario] = useState ({
-                nombre: '',
-                apellido: '',
-                telefono: ''
-        });
+  const handleChange = (e) => {
+    if (modoEdicion === null || modoEdicion === 'info') {
+      setModoEdicion('info');
+      const { name, value } = e.target;
+      setFormulario((prev) => ({
+        ...prev,
+        [name]: value
+      }));
+    }
+  };
 
-        
-        useEffect(() => {
-                if (usuario){
-                        setFormulario({
-                                nombre: usuario.nombre,
-                                apellido: usuario.apellido,
-                                telefono: usuario.telefono
-                        });
-                }
-        }, [usuario]);
+  const guardarCambios = async () => {
+    try {
+      const response = await api.put(`usuarios/actualizacion/${usuario.correo}`, {
+        nombre: formulario.nombre,
+        apellido: formulario.apellido,
+        telefono: formulario.telefono
+      });
 
-
-        
-        const handleChange = (e) => {
-
-                if(modoEdicion === null || modoEdicion ==='info'){
-                        setModoEdicion("info");
-                         const { name, value } = e.target;
-                        setFormulario(prev => ({
-                        ...prev,
-                        [name]: value
-                
-               
-                        }));
-                 }
-        };
-
-        
-        const guardarCambios = async () =>{
-                try{
-
-                const response = await api.put(`usuarios/actualizacion/${usuario.correo}`, {
-                              nombre: formulario.nombre,
-                                apellido: formulario.apellido,
-                                telefono: formulario.telefono
-                });
-
-                if (response){
-                         if (usuario.rolNombre === 'Conductor' ){
-                                 navigate('/inicioconductor', {
-                                 state: { mensaje: `Datos actualizados` }
-                        });
-
-                        }else if (usuario.rolNombre === 'Pasajero' ){
-                                navigate('/iniciopasajero', {
-                                state: { mensaje: `Datos actualizados` }
-                        });
-                        }else{
-                                 setServerError('Rol de usuario no reconocido.');
-                        }
-                }
-
-                         setModoEdicion(null);
- 
-                        
-                        alert("Se guardaron cambios");
-                }catch(error){
-                console.error('error al actualizar:', error)
-      
-
-        }  
-        };
-
-   
-    
-   
-  
-
-    const handleCancel = () => {
-         
-        if (usuario.rolNombre === 'Conductor' ){
-                navigate('/inicioconductor', {
-                 state: { mensaje: `Datos actualizados` }
-         });
-
-        }else if (usuario.rolNombre === 'Pasajero' ){
-                navigate('/iniciopasajero', {
-                 state: { mensaje: `Datos actualizados` }
-         });
-        }else{
-               setServerError('Rol de usuario no reconocido.');
+      if (response) {
+        if (usuario.rolNombre === 'Conductor') {
+          navigate('/inicioconductor', {
+            state: { mensaje: `Datos actualizados` }
+          });
+        } else if (usuario.rolNombre === 'Pasajero') {
+          navigate('/iniciopasajero', {
+            state: { mensaje: `Datos actualizados` }
+          });
+        } else {
+          alert('Rol de usuario no reconocido.');
         }
-        setModoEdicion(null);
-        setParametro(false);
-        setPassword("");
-      
-        };
+      }
 
-  
-        const cambioContra = () =>{
-                if (modoEdicion === null || modoEdicion ==='password'){
-                        setModoEdicion("password");
-                        setParametro(!parametros);
-                }
-                
-                
-        };
-  
-  
+      setModoEdicion(null);
+      alert('Se guardaron cambios');
+    } catch (error) {
+      console.error('error al actualizar:', error);
+    }
+  };
 
-    return (
+  const handleCancel = () => {
+    if (usuario.rolNombre === 'Conductor') {
+      navigate('/inicioconductor', {
+        state: { mensaje: `Operación cancelada` }
+      });
+    } else if (usuario.rolNombre === 'Pasajero') {
+      navigate('/iniciopasajero', {
+        state: { mensaje: `Operación cancelada` }
+      });
+    } else {
+      alert('Rol de usuario no reconocido.');
+    }
+    setModoEdicion(null);
+    setParametro(false);
+    setPassword('');
+  };
 
+  const cambioContra = () => {
+    if (modoEdicion === null || modoEdicion === 'password') {
+      setModoEdicion('password');
+      setParametro(!parametros);
+    }
+  };
 
-        <>
-        
-        <main className="registro-container">
-        <form className="registro-form">   
-
+  return (
+    <main className="registro-container">
+      <form className="registro-form">
         <h1>Editar Datos</h1>
 
-         <div>
-                   <FaUserCircle className="icon-profile" />
+        <div>
+          <FaUserCircle className="icon-profile" />
         </div>
         <div className="campo">
-            <label htmlFor="nombre">Nombre:</label>
-            
-                <input name="nombre" value={formulario.nombre} onChange={handleChange}    disabled={modoEdicion==="password"}  />
-
-        </div>
-
-        <div className="campo">
-                <label htmlFor="apellido">Apellidos:</label>
-
-                 <input name="apellido" value={formulario.apellido} onChange={handleChange} disabled={modoEdicion==="password"}  />
-
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            name="nombre"
+            value={formulario.nombre}
+            onChange={handleChange}
+            disabled={modoEdicion === 'password'}
+          />
         </div>
 
         <div className="campo">
-                <label htmlFor="telefono">Telefono:</label>
-                 <input name="telefono" value={formulario.telefono} onChange={handleChange}  disabled={modoEdicion==="password"}   />
+          <label htmlFor="apellido">Apellidos:</label>
+          <input
+            name="apellido"
+            value={formulario.apellido}
+            onChange={handleChange}
+            disabled={modoEdicion === 'password'}
+          />
+        </div>
 
+        <div className="campo">
+          <label htmlFor="telefono">Telefono:</label>
+          <input
+            name="telefono"
+            value={formulario.telefono}
+            onChange={handleChange}
+            disabled={modoEdicion === 'password'}
+          />
         </div>
         <div className="campo">
-                <button type="button"  onClick={cambioContra} disabled={modoEdicion==="info"} >CambiarContraseña</button>
+          <button type="button" onClick={cambioContra} disabled={modoEdicion === 'info'}>
+            CambiarContraseña
+          </button>
         </div>
 
         {parametros && (
-                <div className="campo">
-                <label htmlFor="password">Ingrese contraseña nueva:</label>
-                 <input type="password" value={password}  name="password" onChange={(event)=> {
-                        setPassword(event.target.value);
+          <div className="campo">
+            <label htmlFor="password">Ingrese contraseña nueva:</label>
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+          </div>
+        )}
 
-
-                }}    />
-
-        </div>
-
-
-        )
-        }
-        
-        
-
-         <div className="registro-botones">
-               
-                <button type="button"  onClick={guardarCambios}>Guardar</button>
-               
-          
+        <div className="registro-botones">
+          <button type="button" onClick={guardarCambios}>
+            Guardar
+          </button>
           <button type="button" className="cancelar" onClick={handleCancel}>
             Cancelar
           </button>
         </div>
-        </form>  
+      </form>
     </main>
-         
-            </>
-
-            
-
-
-
-    );
+  );
 }
 
 export default EditarUsuario;
